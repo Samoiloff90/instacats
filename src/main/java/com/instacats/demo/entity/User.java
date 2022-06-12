08371d3e11027a24c9f7a80255d53d7eb79e1592
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-06-12 20:10:25 +0300

    added setings class User for bd

--- src/main/java/com/instacats/demo/entity/User.java
+++ src/main/java/com/instacats/demo/entity/User.java
@@ -1,26 +1,43 @@
 package com.instacats.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.instacats.demo.entity.enums.ERole;
+import lombok.Data;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@Data
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(unique = true, updatable = true)
     private String username;
+    @Column(nullable = false)
     private String lastname;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
 
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn("user_id"))
     private Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createDate;
 
     @PrePersist
