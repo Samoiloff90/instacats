d740dc14c7e365de0dabf5fc0e0fe8593b6aff10
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-06-12 19:53:15 +0300

    added post class

--- src/main/java/com/instacats/demo/entity/Post.java
+++ src/main/java/com/instacats/demo/entity/Post.java
@@ -1,4 +1,21 @@
 package com.instacats.demo.entity;
 
+import java.time.LocalDateTime;
+import java.util.*;
+
 public class Post {
+    private Long id;
+    private String title;
+    private String caption;
+    private String location;
+    private Integer likes;
+
+    private Set<String> likedUsers = new HashSet<>();
+    private User user;
+    private List<Comment> comments = new ArrayList<>();
+    private LocalDateTime createdDate;
+
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
