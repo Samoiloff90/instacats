2d4e8015ad42dbea4d616f68e4063b3105772893
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-06-12 19:55:06 +0300

    added imageModel class

--- src/main/java/com/instacats/demo/entity/ImageModel.java
+++ src/main/java/com/instacats/demo/entity/ImageModel.java
@@ -1,4 +1,9 @@
 package com.instacats.demo.entity;
 
 public class ImageModel {
+    private Long id;
+    private String name;
+    private byte[] imageBytes;
+    private Long userId;
+    private Long postId;
 }
